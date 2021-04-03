62b8d4727fadf9ced72acddabe9af8a0dba6eefc
Zijie Jin <jzj2035198@outlook.com>
2021-04-04 01:10:19 +0800

    Update CombinePipeline_startwith_ChiDist.sh

--- bin/CombinePipeline_startwith_ChiDist.sh
+++ bin/CombinePipeline_startwith_ChiDist.sh
@@ -20,5 +20,5 @@ Rscript ${codedir}/ChiDist_2.2.0_Combine.R ${numcell} ${FilePath}/ChiDist/${pref
 python ${codedir}/TidyupFusionFinalResult.py ${FilePath}/FinalResult/temp/${prefix}Allresult.txt ${FilePath}/ChimericOut/ > ${FilePath}/FinalResult/temp/${prefix}Allresult_filtered.txt
 python ${codedir}/Results_Filtered2Final.py ${FilePath}/FinalResult/temp/${prefix}Allresult_filtered.txt ${pv} ${fakeprob} ${numcell} ${codedir}/../data/badgene/ > ${FilePath}/FinalResult/${prefix}Final.txt
 python ${codedir}/TidyupFusionFinalResult_FindSupCell.py ${FilePath}/FinalResult/${prefix}Final.txt ${FilePath}/ChimericOut/ > ${FilePath}/FinalResult/${prefix}Final_Cells.txt
-python ${codedir}/ResultLastFiltered.py ${FilePath}/FinalResult/${prefix}Final_Cells.txt ${genepos} ${FilePath}/FinalResult/${prefix}Final_Cells_filtered.txt ${gtf}
+python ${codedir}/ResultLastFiltered.py ${FilePath}/FinalResult/${prefix}Final_Cells.txt ${genepos} ${FilePath}/FinalResult/${prefix}Final_Cells_filtered.txt ${gtf} ${genepos}
 python ${codedir}/ResultFinalOutput.py ${FilePath}/FinalResult/${prefix}Final_Cells_filtered.txt ${gtf} ${FilePath}/ChimericOut/ ${FilePath}/FinalResult/${prefix}FinalOutput
