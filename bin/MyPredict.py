952b7e0d759e96b0322ef2be889e37a3417bd937
Zijie Jin <jzj2035198@outlook.com>
2021-04-04 21:38:42 +0800

    Update MyPredict.py

--- bin/MyPredict.py
+++ bin/MyPredict.py
@@ -12,7 +12,6 @@ import numpy as np
 from keras.utils import to_categorical
 from Model1 import Cla_LSTM
 import os
-import matplotlib.pyplot as plt
 import sys
 
 # os.environ["CUDA_VISIBLE_DEVICES"] = "0"
@@ -44,56 +43,4 @@ AveProb = (Prob[:,0] + Prob_rev[:,0]) / 2
 
 sys.stderr.write(str(np.mean(AveProb)))
 sys.stderr.write('\n')
-
-above5 = 0
-above6 = 0
-above7 = 0
-above8 = 0
-above9 = 0
-above0 = 0
-above1 = 0
-above2 = 0
-above3 = 0
-above4 = 0
-for i in range(len(AveProb)):
-    outfile.write(str(Prob[i,0]) + '\t' + str(Prob_rev[i,0]) + '\t' + str(AveProb[i]) + '\n')
-    if i >= 0:
-        above0 += 1
-    if i >= 0.1:
-        above1 += 1
-    if i >= 0.2:
-        above2 += 1
-    if i >= 0.3:
-        above3 += 1
-    if i >= 0.4:
-        above4 += 1
-    if i >= 0.5:
-        above5 += 1
-    if i >= 0.6:
-        above6 += 1
-    if i >= 0.7:
-        above7 += 1
-    if i >= 0.8:
-        above8 += 1
-    if i >= 0.9:
-        above9 += 1
-sys.stderr.write(str(above0))
-sys.stderr.write('\n')
-sys.stderr.write(str(above1))
-sys.stderr.write('\n')
-sys.stderr.write(str(above2))
-sys.stderr.write('\n')
-sys.stderr.write(str(above3))
-sys.stderr.write('\n')
-sys.stderr.write(str(above4))
-sys.stderr.write('\n')
-sys.stderr.write(str(above5))
-sys.stderr.write('\n')
-sys.stderr.write(str(above6))
-sys.stderr.write('\n')
-sys.stderr.write(str(above7))
-sys.stderr.write('\n')
-sys.stderr.write(str(above8))
-sys.stderr.write('\n')
-sys.stderr.write(str(above9))
 outfile.close()
